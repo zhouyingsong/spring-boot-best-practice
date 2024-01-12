1c725705de43f1afde57e193e127b014a6ea4ec0
zhouyingsong <1312524815@qq.com>
2024-01-12 16:54:18 +0800

    取消打印

--- spring-boot-aop/src/main/java/cn/javastack/springboot/aop/aspect/CalcAspect.java
+++ spring-boot-aop/src/main/java/cn/javastack/springboot/aop/aspect/CalcAspect.java
@@ -18,30 +18,30 @@ public class CalcAspect {
 
     @Before("pointcut()")
     public void before() {
-        System.out.println("********** @Before 前置通知");
+        // System.out.println("********** @Before 前置通知");
     }
 
     @After("pointcut()")
     public void after() {
-        System.out.println("******** @After 后置通知");
+        // System.out.println("******** @After 后置通知");
     }
 
     @AfterReturning("pointcut()")
     public void afterReturning() {
-        System.out.println("******* @AfterReturning 返回通知");
+        // System.out.println("******* @AfterReturning 返回通知");
     }
 
     @AfterThrowing("pointcut()")
     public void afterThrowing() {
-        System.out.println("******** @AfterThrowing 异常通知");
+        // System.out.println("******** @AfterThrowing 异常通知");
     }
 
     @Around("pointcut()")
     public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         Object result;
-        System.out.println("环绕通知之前");
+        // System.out.println("环绕通知之前");
         result = proceedingJoinPoint.proceed();
-        System.out.println("环绕通知之后");
+        // System.out.println("环绕通知之后");
         return result;
     }
 
